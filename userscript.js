0ea27e841b3d3594f1411e392e44b6fad64a6ea1
s626ch <coolguygameplayer52@gmail.com>
2023-08-12 18:32:21 -0400

    wait hold on
    why did i commit the original script instead of the rewrite oops i need sleep

--- userscript.js
+++ userscript.js
@@ -1,63 +1,48 @@
 // ==UserScript==
 // @name         better bajookie tab names
-// @version      0.3
+// @version      0.6.26
 // @description  yeah
 // @author       stitch
 // @match        https://bajookie.bingle.plus/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bingle.plus
 // @grant        none
 // ==/UserScript==
-
-/*
-    if (window.location.href.startsWith("")) {
-        document.title = " - Bajookie";
-    };
-*/
-
 (function() {
     'use strict';
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Game/")) {
-        document.title = document.querySelector("body > div.container.w-50.bg-dark.text-white.border.rounded.pb-3 > div.row > div.p-2.desccol > h3").innerText + " - Bajookie";
+    // map links to data that i need to get proper doc title shit from
+    // also store base url here bcz looks nicer
+    const urlprfx = "https://bajookie.bingle.plus/"
+    const urlToTitle = {
+        [urlprfx + "Game/"]: getTitleFromSelector("body > div.container.w-50.bg-dark.text-white.border.rounded.pb-3 > div.row > div.p-2.desccol > h3"),
+        [urlprfx + "Games"]: "Games",
+        [urlprfx + "Catalog"]: "Catalog",
+        [urlprfx + "Item/"]: getTitleFromSelector("body > div > div.row > div.p-2.desccol > h3"),
+        [urlprfx + "Home"]: "Home",
+        [urlprfx + "Avatar"]: "Avatar",
+        [urlprfx + "Users"]: "Users",
+        [urlprfx + "Users/"]: getTitleFromSelector("body > div > div:nth-child(1) > div.col.border-right.p-2 > div > div.col > h4"),
+        [urlprfx + "Groups"]: "Groups",
+        [urlprfx + "Group/"]: getTitleFromSelector("body > div > div.row.border-bottom > div > div.row > div.col > h4")
     };
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Games")) {
-        //document.title = document.querySelector("body > div > h3").innerText + " - Bajookie";
-        document.title = "Games - Bajookie";
-    };
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Catalog")) {
-        //document.title = document.querySelector("body > div > div > div.col.border-right.pb-2 > h3").innerText + " - Bajookie";
-        document.title = "Catalog - Bajookie";
-    };
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Item/")) {
-        document.title = document.querySelector("body > div > div.row > div.p-2.desccol > h3").innerText + " - Bajookie";
-    };
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Home")) {
-        document.title = "Home - Bajookie";
-    };
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Avatar")) {
-        document.title = "Avatar - Bajookie";
-    };
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Users")) {
-        document.title = "Users - Bajookie";
-    };
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Users/")) {
-        document.title = document.querySelector("body > div > div:nth-child(1) > div.col.border-right.p-2 > div > div.col > h4").innerText + " - Bajookie";
-    };
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Groups")) {
-        document.title = "Groups - Bajookie";
-    };
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Group/")) {
-        document.title = document.querySelector("body > div > div.row.border-bottom > div > div.row > div.col > h4").innerText + " - Bajookie";
-    };
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Gifts")) {
-        document.title = "Gifts - Bajookie";
-    };
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Money")) {
-        document.title = "Transactions - Bajookie";
-    };
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Create")) {
-        document.title = "Develop - Bajookie";
-    };
-    if (window.location.href.startsWith("https://bajookie.bingle.plus/Settings")) {
-        document.title = "Settings - Bajookie";
-    };
+    const currentURL = window.location.href;
+    const defaultTitle = "Bajookie"; // fallback if for some fucking reason i forgot something
+    // iterate through mappings, break when match is found
+    let matchedPrefix = null;
+    for (const urlPrefix in urlToTitle) {
+        if (currentURL.startsWith(urlPrefix)) {
+            matchedPrefix = urlPrefix;
+            break;
+        }
+    }
+    // set to matched prefix, else fallback to le "what the fuck i forgot something"
+    if (matchedPrefix) {
+        document.title = `${urlToTitle[matchedPrefix]} - Bajookie`;
+    } else {
+        document.title = `${defaultTitle}`;
+    }
 })();
+// fetch title from page for things that need it, ex: games, item, user, etc.
+function getTitleFromSelector(selector) {
+    const titleElement = document.querySelector(selector);
+    return titleElement ? titleElement.innerText : "";
+}
